06369806b7386f8dc3b9740ba7540dd34bdbe085
Nationjr <136012845+Nationjr@users.noreply.github.com>
2025-11-10 09:36:17 +0100

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message = "Hello, Base! 👋";
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
